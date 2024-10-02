d5ef9d1769d96088478e4efb9be1934786cd85b7
Senchenko Nikita <63493300+Senchenko-Dev@users.noreply.github.com>
2024-10-02 10:22:17 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,32 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
+    "fmt"
+    "net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "Hello, World!")
+}
+
 func main() {
-	http.HandleFunc("/", helloServer)
-	log.Println("Server started on 8080.")
-	http.ListenAndServe(":8080", nil)
+    http.HandleFunc("/", helloHandler)
+    http.ListenAndServe(":8080", nil)
 }
-
-func helloServer(w http.ResponseWriter, r *http.Request) {
-	name := os.Getenv("NAME")
-	if name == "" {
-		name = "v0.0.16"
-	}
-	log.Println(fmt.Sprintf("Hello, %s!", name))
-	
-	w.Header().Set("Content-Type", "application/json")
-	response := []byte(fmt.Sprintf("{\"test\": \"Hello, %s!\"}", name))
-	_, err := w.Write(response)
-	if err != nil {
-        	// Handle the error gracefully
-	}
-}
-
-
-
